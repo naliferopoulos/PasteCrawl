c54cae42a673b13aa16c852d56691ad344be2d6c
Nick Aliferopoulos <aliferopoulos@icloud.com>
2020-01-09 18:40:25 +0200

    Cosmetic changes and avoid crash on ban.

--- pastecrawl.py
+++ pastecrawl.py
@@ -5,42 +5,43 @@ from bs4 import BeautifulSoup
 while True:
     # Links to crawl
     links = []
-
-    # Fetch the page and parse it
-    page = requests.get("http://pastebin.com/archive")
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    # Fecth the main table
-    table = soup.find('table', {'class': 'maintable'})
-
-    # Fetch the rows
-    rows = table.findAll('tr')
-
-    for tr in rows:
-        #print("\tFound Row")
-        # Fetch the columns
-        cols = tr.findAll('td')
-
-        for td in cols:
-            #print("\t\tFound Column")
-            # Fetch the links    
-            hrefs = td.findAll('a')
-            
-            for a in hrefs:
-                #print("\t\t\tFound Link")
-                # Fetch the content    
-                link = a.get('href')
-                
-                # Skip over default ones
-                if "archive" not in link:
-                    links.append(link) 
     
+    try:
+
+        # Fetch the page and parse it
+        page = requests.get("http://pastebin.com/archive")
+        soup = BeautifulSoup(page.content, 'html.parser')
+    
+        # Fetch the main table
+        table = soup.find('table', {'class': 'maintable'})
+
+        # Fetch the rows
+        rows = table.findAll('tr')
+
+        for tr in rows:
+            # Fetch the columns
+            cols = tr.findAll('td')
+
+            for td in cols:
+                # Fetch the links    
+                hrefs = td.findAll('a')
+                
+                for a in hrefs:
+                    # Fetch the content    
+                    link = a.get('href')
+                    
+                    # Skip over default ones
+                    if "archive" not in link:
+                        links.append(link) 
+    except:
+        print("Failed to fetch latest content. Potential timeout by service. This may indicate a temporary ban!")
+        break
+
     # Crawl each link
     for link in links:
         print("--> " + link)
         try:
             crawl = requests.get("http://pastebin.com/raw" + link)
-            #with open('./crawls/' + link + '_' + str(int(time.time())) + ".txt", "w") as f:
             with open('./crawls/' + str(int(time.time())) + ".txt", "w") as f:
                 f.write(crawl.text)
         except:
