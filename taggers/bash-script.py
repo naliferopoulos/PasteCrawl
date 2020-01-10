954e51dae0d4b9b860d3b24aff5e1281d92a35e6
naliferopoulos <aliferopoulos@icloud.com>
2020-01-10 10:38:26 +0200

    Removed unused or

--- taggers/bash-script.py
+++ taggers/bash-script.py
@@ -1,6 +1,6 @@
 class Tagger:
     def tag(text):
-        if "#!/bin/bash" in text.lower() or "-eq" in text.lower() or "#! /bin/bash" in text.lower() or:
+        if "#!/bin/bash" in text.lower() or "-eq" in text.lower() or "#! /bin/bash" in text.lower():
             return ["bash-script"]
         else:
             return []
